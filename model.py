ac271817048f017d8c2d54b3d2c84703d5a37ded
Qianxy <xyqian67@gmail.com>
2024-05-02 18:27:00 -0700

    Use LeNet model

--- model.py
+++ model.py
@@ -8,8 +8,16 @@ mnist = tf.keras.datasets.mnist.load_data()
 x_train, x_test = x_train / 255., x_test / 255.
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=[28, 28]),
-    tf.keras.layers.Dense(256, activation="relu"),
+    tf.keras.layers.Conv2D(
+        filters=6, kernel_size=(5, 5),
+        padding="same", activation="relu", input_shape=(28, 28, 1),
+    ),
+    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), activation="relu"),
+    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(120, activation="relu"),
+    tf.keras.layers.Dense(84, activation="relu"),
     tf.keras.layers.Dense(10, activation="softmax"),
 ])
 
@@ -22,11 +30,12 @@ model.compile(
 )
 
 model.fit(
-    x_train, y_train, epochs=10,
+    x_train, y_train, epochs=20,
     validation_data=(x_test, y_test),
 )
 
 spec = tf.TensorSpec(shape=(None, 28, 28), dtype=tf.float32, name="input")
+model.output_names = ['output']
 model_proto, _ = tf2onnx.convert.from_keras(
     model,
     input_signature=(spec,),
